99992caff413fd4339251a1a85ea4a49f2d11073
cjr8020 <cjr8020@gmail.com>
2019-03-11 16:45:32 -0700

    seed repo

---
+++ src/main/java/com/example/sbcloudvaultdemo/SbCloudVaultDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbcloudvaultdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCloudVaultDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCloudVaultDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbcloudvaultdemo/SbCloudVaultDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sbcloudvaultdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbCloudVaultDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
